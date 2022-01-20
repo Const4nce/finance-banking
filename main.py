d87840d6d49e023691a28e8a0c39cf8dc51d7768
Const4nce <k.archeou@gmail.com>
2022-01-20 18:43:08 +0100

    compound-interest-added

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import basic_functions
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # value = future_value(1000, 0.005, 12, present_value = 1061.68)
-    value = basic_functions.future_value_factor(0.01, 12, future_value=500)
+    value = basic_functions.continuous_compound_interest(10000, 10.25, 5, ending_balance=False)
 
     print(value)
